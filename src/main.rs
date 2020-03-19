630e5f2182126e26a5d2834d5a89d2b4c78240c8
Robin Monjo <robinmonjo@gmail.com>
2020-03-19 20:08:42 +0100

    static file handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate env_logger;
-use actix_web::{middleware::Logger, get, App, HttpServer, Responder};
 
-#[get("/")]
+use actix_web::{middleware::Logger, get, App, HttpServer, Responder};
+use actix_files as fs;
+
+#[get("/lol")]
 async fn root() -> impl Responder {
     return "hello world"
 }
@@ -15,6 +17,10 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .service(root)
+            .service(
+                // static files
+                fs::Files::new("/", "./static/").index_file("index.html"),
+            )
     })
     .bind("127.0.0.1:8080")?
     .run()
